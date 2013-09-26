bea3caa668aaf5648a4fdb28fc715c176280b77a
Piotr Żurkowski <kareth92@gmail.com>
2013-09-27 00:16:14 +0200

    fix trail drawing

--- src/client/trail.cpp
+++ src/client/trail.cpp
@@ -1,11 +1,12 @@
 #include <client/trail.h>
 using glm::vec3;
 using glm::mat4;
+using std::swap;
 
 namespace client{
 
 Trail::Trail(PLAYER_INFO position, std::vector<TURN_INFO>& turns, int color)
-  : position_(position), turns_(turns), color_(color) {
+  : position_(position), turns_(turns), color_(color), c_(0), v_(0), t_(0), n_(0){
 
 }
 
@@ -111,48 +112,78 @@ void Trail::Draw(){
       end = vec3(turns_[i+1].move.x, turns_[i+1].move.y, 0);
     else
       end = vec3(position_.x, position_.y, 0);
-
     vec3 size = end - begin;
-
 
     // If we draw along X
     if( fabs(size.x) > fabs(size.y)) {
       std::swap(size.x, size.y);
-      mat4 M;
-      if (size.y < 0)
-        M = World::transform(begin + vec3(0.05f, -0.05f,0), 0,0,90);
-      else
-        M = World::transform(begin + vec3(-0.05f, 0.05f,0) , 0,0,-90);
-      size.y = fabs(size.y) + 0.1;
-
-      DrawLine(M, size, tex);
+      if (size.y < 0){
+        begin += vec3(0.05f, 0.05f,0);
+        end += vec3(-0.05f, -0.05f,0);
+        swap(begin, end);
+      }
+      else{
+        begin += vec3(-0.05f, -0.05f,0);
+        end += vec3(0.05f, 0.05f,0);
+      }
+      AddX(begin, end);
     }
 
     // If we draw along Y
     else if( fabs(size.x) < fabs(size.y)) {
-      mat4 M;
-      if (size.y < 0)
-        M = World::transform(begin + vec3(0.05f, 0.05f,0), 0,0,180);
-      else
-        M = World::transform(begin + vec3(-0.05f, -0.05f,0), 0,0,0);
-      size.y = fabs(size.y) + 0.1;
-
-      DrawLine(M, size, tex);
+      if (size.y < 0){
+        begin += vec3(0.05f, 0.05f,0);
+        end += vec3(-0.05f, -0.05f,0);
+        swap(begin, end);
+      }
+      else{
+        begin += vec3(-0.05f, -0.05f,0);
+        end += vec3(0.05f, 0.05f,0);
+      }
+      AddY(begin, end);
     }
   }
-
+  DrawAll(tex);
 }
 
-void Trail::DrawLine(mat4 M, vec3 dimensions, std::string texture){
+
+void Trail::AddY(vec3 begin, vec3 end) {
+  double length = end.y - begin.y;
   for(int i = 0; i < trailVerticesCount * 3; i+=3){
-    //trailVertices[i+0] *= dimensions.x;
-    trailVertices[i+1] *= dimensions.y;
-    //trailVertices[i+2] *= dimensions.z;
+    v_.push_back(trailVertices[i] + begin.x);
+    v_.push_back(trailVertices[i+1] * length + begin.y);
+    v_.push_back(trailVertices[i+2]);
   }
+  for(int i = 0; i < trailVerticesCount / 4 * 8; i++) {
+    t_.push_back(trailTexCoords[i]);
+  }
+  for(int i = 0; i < trailVerticesCount * 3; i++) {
+    n_.push_back(trailNormals[i]);
+  }
+  c_+= trailVerticesCount;
+}
 
-  glLoadMatrixf(glm::value_ptr(World::getV() * M));
+
+void Trail::AddX(vec3 begin, vec3 end) {
+  swap(begin.x, begin.y);
+  swap(end.x, end.y);
+  AddY(begin, end);
+
+  for(int i = v_.size() - (trailVerticesCount * 3); i < v_.size(); i+=3){
+    swap(v_[i], v_[i+1]);
+  }
+  for(int i = n_.size() - ( trailVerticesCount * 3); i < n_.size(); i+=3) {
+    swap(n_[i], n_[i+1]);
+  }
+}
 
 
+void Trail::DrawAll(std::string texture){
+  printf("%d\n",(int)v_.size());
+  printf("%d\n",(int)t_.size());
+  printf("%d\n",(int)n_.size());
+  printf("%d\n",c_);
+  glLoadMatrixf(glm::value_ptr(World::getV() * World::center()));
 
     TextureManager::setTexture(texture);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
@@ -171,10 +202,10 @@ void Trail::DrawLine(mat4 M, vec3 dimensions, std::string texture){
   glEnableClientState(GL_TEXTURE_COORD_ARRAY);
   glEnableClientState(GL_NORMAL_ARRAY);
 
-  glVertexPointer(3,GL_FLOAT,0,trailVertices);
-  glTexCoordPointer( 2, GL_FLOAT, 0, trailTexCoords);
-  glNormalPointer(GL_FLOAT, 0,trailNormals);
-  glDrawArrays(GL_QUADS,0, trailVerticesCount);
+  glVertexPointer(3,GL_FLOAT,0, &v_[0]);
+  glTexCoordPointer( 2, GL_FLOAT, 0, &t_[0]);
+  glNormalPointer(GL_FLOAT, 0, &n_[0]);
+  glDrawArrays(GL_QUADS,0, c_);
 
   glDisableClientState(GL_NORMAL_ARRAY);
   glDisableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -182,13 +213,8 @@ void Trail::DrawLine(mat4 M, vec3 dimensions, std::string texture){
 
   //glBindTexture(GL_TEXTURE_2D, NULL);
 
-  for(int i = 0; i < trailVerticesCount * 3; i+=3)
-    if(trailVertices[i+1] != 0) trailVertices[i+1] = 1;
-
   glDisable(GL_TEXTURE_2D);
   glDisable(GL_BLEND);
-
-
 }
 
 }  // namespace client
